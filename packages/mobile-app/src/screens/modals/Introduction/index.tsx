27af1079ddd46d9909e72c5659ef17577ce9dfab
Manuel Ruck <github.com@manuelruck.de>
2019-12-10 19:04:29 +0700

    prepare react-native-snap-carousel

--- packages/mobile-app/src/screens/modals/Introduction/index.tsx
+++ packages/mobile-app/src/screens/modals/Introduction/index.tsx
@@ -7,9 +7,9 @@ import { RootStackParamList } from '../../../routes';
 
 import { Slide } from '@democracy-deutschland/mobile-ui/src/components/Introduction';
 import { slidesData } from '@democracy-deutschland/mobile-ui/src/components/Introduction/data';
-import { Pager } from '@democracy-deutschland/mobile-ui/src/components/Pager';
 import { getSlides } from './utils/getSlides';
 import { InitialStateContext } from '../../../context/InitialStates';
+import Carousel, { CarouselProps } from 'react-native-snap-carousel';
 
 const SafeAreaView = styled.SafeAreaView`
   flex: 1;
@@ -49,33 +49,36 @@ const Introduction: FC<Props> = ({ route }) => {
     registered: isVerified,
   });
 
+  const _renderItem: CarouselProps<Slide>['renderItem'] = ({ item, index }) => {
+    return (
+      <Slide
+        key={item.head.title}
+        head={item.head}
+        images={item.images}
+        isNew={item.isNew}
+        verify={
+          index + 1 === Object.keys(slidesData).length
+            ? verifyAction
+            : undefined
+        }
+        nextSlide={
+          // TODO fix android next button click. does not work correctly
+          index + 1 === Object.keys(slidesData).length
+            ? finishAction
+            : undefined
+        }
+      />
+    );
+  };
+
   return (
     <SafeAreaView testID="Introduction">
-      <Pager
-        nextButton
-        nextText="Weiter"
-        finishText="Los geht's"
-        finishAction={finishAction}>
-        {slides.map((slide, i) => (
-          <Slide
-            key={slide.head.title}
-            head={slide.head}
-            images={slide.images}
-            isNew={slide.isNew}
-            verify={
-              i + 1 === Object.keys(slidesData).length
-                ? verifyAction
-                : undefined
-            }
-            nextSlide={
-              // TODO fix android next button click. does not work correctly
-              i + 1 === Object.keys(slidesData).length
-                ? finishAction
-                : undefined
-            }
-          />
-        ))}
-      </Pager>
+      <Carousel<Slide>
+        data={slides}
+        renderItem={_renderItem}
+        sliderWidth={300}
+        itemWidth={300}
+      />
     </SafeAreaView>
   );
 };
